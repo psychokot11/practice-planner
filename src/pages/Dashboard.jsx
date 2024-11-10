034d774fa0f222148bf9c97d3a3a58e79c4bd3fb
Zofia Stawarska <z.stawarska@gmail.com>
2024-11-10 20:01:43 +0100

    dashboard styles

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,16 @@
+import ButtonTile from "../ui/ButtonTile";
+
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <div className="flex gap-10 justify-around my-10">
+        <ButtonTile color="pink">PLANS</ButtonTile>
+        <ButtonTile>DRILLS</ButtonTile>
+        <ButtonTile>CREATE PLAN</ButtonTile>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
---
+++ src/ui/ButtonTile.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+function ButtonTile({ children, onClick, disabled = false }) {
+  return (
+    <button
+      className="flex-1 text-3xl font-medium text-neutral-700 bg-violet-400 border-4 border-violet-500 rounded-lg p-2 transition-all duration-100 ease-in py-10"
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default ButtonTile;
+
+ButtonTile.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+  color: PropTypes.oneOf(["red", "green", "blue", "yellow"]),
+};
