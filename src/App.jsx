39238aaaeba674d26d1cf85320091a89e45a50c0
Zofia Stawarska <z.stawarska@gmail.com>
2024-11-10 21:44:09 +0100

    install react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppLayout from "./ui/AppLayout";
 import Dashboard from "./pages/Dashboard";
 import Plans from "./pages/Plans";
 import Drills from "./pages/Drills";
 import PageNotFound from "./pages/PageNotFound";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClinet = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60,
+    },
+  },
+});
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="plans" element={<Plans />} />
-          <Route path="drills" element={<Drills />} />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClinet}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="plans" element={<Plans />} />
+            <Route path="drills" element={<Drills />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
