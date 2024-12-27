f2a9663e00255b44059af39d2229a4fba987f48f
Zofia Stawarska <z.stawarska@gmail.com>
2024-12-27 11:15:39 +0100

    404 page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,17 @@
+import { NavLink } from "react-router-dom";
+
 function PageNotFound() {
-  return <>404 not found</>;
+return (
+  <div className="max-w-main mx-auto flex gap-12 flex-col">
+    <div className="min-h-screen flex flex-grow items-center justify-center bg-gray-50">
+      <div className="rounded-lg bg-white p-8 text-center shadow-xl">
+        <h1 className="mb-4 text-4xl font-bold">404</h1>
+        <p className="text-gray-600">Oops! The page you are looking for could not be found.</p>
+        <NavLink to="/" className="mt-4 inline-block rounded bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600"> Go back to Home </NavLink>
+      </div>
+    </div>
+  </div>
+  )
 }
 
 export default PageNotFound;
