00fd18beeb2b2b12027f92ecf1312d4f2f94931f
Zofia Stawarska <z.stawarska@gmail.com>
2024-12-27 10:59:39 +0100

    dashboard buttons

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,15 @@
 import ButtonTile from "../ui/ButtonTile";
+import { FcPlanner, FcIdea, FcAddDatabase } from "react-icons/fc";
+
 
 function Dashboard() {
   return (
     <div>
       <h1>Dashboard</h1>
       <div className="flex gap-10 justify-around my-10">
-        <ButtonTile>PLANS</ButtonTile>
-        <ButtonTile>DRILLS</ButtonTile>
-        <ButtonTile>CREATE PLAN</ButtonTile>
+        <ButtonTile><FcPlanner className="size-10" /> PLANS</ButtonTile>
+        <ButtonTile><FcIdea  className="size-10" /> DRILLS</ButtonTile>
+        <ButtonTile><FcAddDatabase  className="size-10" /> CREATE PLAN</ButtonTile>
       </div>
     </div>
   );
--- src/ui/ButtonTile.jsx
+++ src/ui/ButtonTile.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 function ButtonTile({ children, onClick, disabled = false }) {
   return (
     <button
-      className="flex-1 text-3xl font-medium text-neutral-700 bg-violet-400 border-4 border-violet-500 rounded-lg p-2 transition-all duration-100 ease-in py-10"
+      className="flex flex-col items-center gap-3 text-lg"
       onClick={onClick}
       disabled={disabled}
     >
