4b9a564c3acd0a8add6755d62a0bf9f962efbb12
Zofia Stawarska <z.stawarska@gmail.com>
2024-12-29 20:27:56 +0100

    checked tags

--- src/features/drills/CreateDrillForm.jsx
+++ src/features/drills/CreateDrillForm.jsx
@@ -125,7 +125,8 @@ function CreateDrillForm({ drill, type, onClose }) {
                                                         <input type="checkbox" 
                                                             id={tag.id} 
                                                             name={tag.name} 
-                                                            value={tag.name} 
+                                                            value={tag.name}
+                                                            defaultChecked={type === "edit" && drill.tags.includes(tag.name)}
                                                             className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
                                                         <label htmlFor={tag.id} 
                                                             className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{tag.name}</label>
