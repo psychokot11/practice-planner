36f85a5bc42cd00d0b96f835c85fc673b84f4c38
Zofia Stawarska <z.stawarska@gmail.com>
2025-01-15 22:40:26 +0100

    random plan button

--- src/features/plans/CreateRandomPlanForm.jsx
+++ src/features/plans/CreateRandomPlanForm.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import Button from "../../ui/Button";
 
 function CreateRandomPlanForm({ drills, tags, onClose }) {
     console.log(drills, tags);
@@ -35,11 +36,13 @@ function CreateRandomPlanForm({ drills, tags, onClose }) {
                                         />
                                 </div>
                             </div>
-                            <button type="submit"
+                            <Button
+                                type="submit"
                                 disabled={false}
-                                className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            >
                                 Create random plan
-                            </button>
+                            </Button>
+                            
                         </form>
                     </div>
                 </div>
