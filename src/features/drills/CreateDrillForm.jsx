dd2d00022d3f1c6cf947bd5ae80afbc01847c92d
Zofia Stawarska <z.stawarska@gmail.com>
2025-01-23 19:32:28 +0100

    further button changes

--- src/features/drills/CreateDrillForm.jsx
+++ src/features/drills/CreateDrillForm.jsx
@@ -7,6 +7,7 @@ import TagsCheckboxList from "../tags/TagsCheckboxList";
 import Button from "../../ui/Button";
 import PlusIcon from "../../ui/PlusIcon";
 import DropdownArrow from "../../ui/DropdownArrow";
+import XIcon from "../../ui/XIcon";
 
 function CreateDrillForm({ drill, type, onClose }) {
   const { tags, isLoading } = useTags();
@@ -96,28 +97,10 @@ function CreateDrillForm({ drill, type, onClose }) {
               <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
                 {type === "create" ? "Create new drill" : "Edit drill"}
               </h3>
-              <button
-                type="button"
-                onClick={onClose}
-                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-              >
-                <svg
-                  className="w-3 h-3"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 14"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                  />
-                </svg>
+              <Button onClick={onClose} type="button" subtype="close">
+                <XIcon />
                 <span className="sr-only">Close modal</span>
-              </button>
+              </Button>
             </div>
             <form
               onSubmit={handleSubmit(onSubmit, onError)}
