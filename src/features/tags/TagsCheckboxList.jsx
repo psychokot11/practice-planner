75e9bfd057665ee58b342432a79dc6a66431a298
Zofia Stawarska <z.stawarska@gmail.com>
2025-01-23 22:24:53 +0100

    tags checkbox list

--- src/features/tags/TagsCheckboxList.jsx
+++ src/features/tags/TagsCheckboxList.jsx
@@ -1,30 +1,60 @@
-import Spinner from "../../ui/Spinner";
+import Button from '../../ui/buttons/Button'
+import DropdownArrow from '../../ui/icons/DropdownArrow'
 
-function TagsCheckboxList({ tags, isLoadingTags, handleTagChange, type, plan, drill }) {
-    const defaultItem = plan || drill;
-    
-    if (isLoadingTags) return <Spinner />;
+function TagsCheckboxList({
+    tags,
+    handleTagChange,
+    type,
+    plan,
+    drill,
+    handleDropdownToggle,
+    isDropdownOpen,
+}) {
+    const defaultItem = plan || drill
 
     return (
-        <ul className="p-3 space-y-3 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownCheckboxButton">
-            {tags.map((tag) => (
-                <li key={tag.id}>
-                    <div className="flex items-center">
-                        <input  
-                            onChange={handleTagChange}
-                            type="checkbox" 
-                            id={tag.id} 
-                            name={tag.name} 
-                            value={tag.name}
-                            defaultChecked={type === "edit" && defaultItem.tags.includes(tag.name)}
-                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                        <label htmlFor={tag.id} 
-                            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{tag.name}</label>
-                    </div>
-                </li>
-            ))}
-        </ul>
+        <div className="relative">
+            <Button onClick={handleDropdownToggle} type="button" flex fullWidth>
+                Choose main focus areas
+                <DropdownArrow />
+            </Button>
+            <div
+                className={`${
+                    !isDropdownOpen && 'hidden'
+                } absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600 w-full`}
+            >
+                <ul
+                    className="p-3 space-y-3 text-sm text-gray-700 dark:text-gray-200 max-h-40 overflow-y-auto"
+                    aria-labelledby="dropdownCheckboxButton"
+                >
+                    {tags.map((tag) => (
+                        <li key={tag.id}>
+                            <div className="flex items-center">
+                                <input
+                                    onChange={handleTagChange}
+                                    type="checkbox"
+                                    id={tag.id}
+                                    name={tag.name}
+                                    value={tag.name}
+                                    defaultChecked={
+                                        type === 'edit' &&
+                                        defaultItem.tags.includes(tag.name)
+                                    }
+                                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                                />
+                                <label
+                                    htmlFor={tag.id}
+                                    className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                >
+                                    {tag.name}
+                                </label>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     )
 }
 
-export default TagsCheckboxList;
+export default TagsCheckboxList
