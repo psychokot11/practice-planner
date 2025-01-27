217c76a26e7ab6dcb557e825b5554fbccf77cf14
Zofia Stawarska <z.stawarska@gmail.com>
2025-01-27 21:10:08 +0100

    useSelectTag

---
+++ src/features/tags/useSelectTag.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+export function useSelectTag({ plan }) {
+    const [isTagsDropdownOpen, setIsTagsDropdownOpen] = useState(false)
+    const [selectedTags, setSelectedTags] = useState(
+        plan?.tags ? plan.tags : []
+    )
+
+    function handleTagDropdownToggle(item) {
+        item === 'tags' && setIsTagsDropdownOpen(!isTagsDropdownOpen)
+    }
+
+    function handleTagChange(event) {
+        const { value, checked } = event.target
+
+        let tagsArray = []
+
+        if (typeof selectedTags === 'string') {
+            tagsArray = selectedTags.split(',').map((tag) => tag.trim())
+        }
+
+        if (checked) {
+            if (!tagsArray.includes(value)) {
+                tagsArray.push(value)
+            }
+            setSelectedTags(tagsArray.join(', '))
+        } else {
+            const filteredTags = tagsArray.filter((tag) => tag !== value)
+            setSelectedTags(filteredTags.join(', '))
+        }
+    }
+
+    return {
+        handleTagDropdownToggle,
+        isTagsDropdownOpen,
+        handleTagChange,
+        selectedTags,
+    }
+}
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,17 +1,18 @@
-import MainNav from "./MainNav";
+import MainNav from './MainNav'
 
 function Sidebar({ isMenuOpen, handleMenu }) {
-  const sidebarClasses = isMenuOpen ? "translate-x-0" : "-translate-x-full";
-  return (
-    <aside 
-      id="logo-sidebar" 
-      className={`${sidebarClasses} fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700`} 
-      aria-label="Sidebar">
-      <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-        <MainNav handleMenu={handleMenu}/>
-      </div>
-    </aside>
-  );
+    const sidebarClasses = isMenuOpen ? 'translate-x-0' : '-translate-x-full'
+    return (
+        <aside
+            id="logo-sidebar"
+            className={`${sidebarClasses} fixed top-0 left-0 z-30 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700`}
+            aria-label="Sidebar"
+        >
+            <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+                <MainNav handleMenu={handleMenu} />
+            </div>
+        </aside>
+    )
 }
 
-export default Sidebar;
+export default Sidebar
