a14894e9b31f401472cfcd196878f9629aee329b
Zofia Stawarska <z.stawarska@gmail.com>
2025-04-06 19:15:12 +0200

    style tooltip

--- src/ui/Tooltip.jsx
+++ src/ui/Tooltip.jsx
@@ -5,7 +5,7 @@ function Tooltip({ children, message }) {
             <div className="absolute z-2 left-1/2 top-5 ml-auto mr-auto min-w-max -translate-x-1/2 scale-0 transform rounded-lg px-3 py-2 text-xs font-medium transition-all duration-500 group-hover:scale-100 z-30">
                 <div className="flex max-w-xs flex-col items-center shadow-lg">
                     <div className="clip-bottom h-3 w-3 bg-gray-600 rotate-45"></div>
-                    <div className="rounded bg-gray-600 p-2 text-center text-xs text-white  -mt-2">
+                    <div className="rounded bg-gray-600 p-2 text-center text-sm text-white font-normal -mt-2">
                         {message}
                     </div>
                 </div>
