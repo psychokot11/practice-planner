959dfc43ba0657b0226b9b7c4be981e98ab4e7b1
Zofia Stawarska <z.stawarska@gmail.com>
2025-08-15 15:17:22 +0200

    scroll

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,26 +1,30 @@
-import { useState } from "react";
-import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import Sidebar from "./Sidebar";
+import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from './Header'
+import Sidebar from './Sidebar'
 
 function AppLayout() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  
-  return (
-    <div className="pt-[64px] lg:pt-[72px] pb-16">
-      <Sidebar isMenuOpen={isMenuOpen} handleMenu={() => setIsMenuOpen(!isMenuOpen)}/>
-      <div className="grid">
-        <Header handleMenu={() => setIsMenuOpen(!isMenuOpen)}/> 
-        <main
-          onClick={() => isMenuOpen && setIsMenuOpen(false)} 
-          className="bg-neutral-50 py-16 px-4 md:py-16 md:px-4 lg:p-16 lg:ml-64 h-screen overflow-x-auto">
-          
-            <Outlet />
-          
-        </main>
-      </div>
-    </div>
-  );
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    return (
+        <>
+            <Sidebar
+                isMenuOpen={isMenuOpen}
+                handleMenu={() => setIsMenuOpen(!isMenuOpen)}
+            />
+            <div className="grid">
+                <Header handleMenu={() => setIsMenuOpen(!isMenuOpen)} />
+                <main
+                    onClick={() => isMenuOpen && setIsMenuOpen(false)}
+                    className="bg-neutral-50 py-16 px-4 md:px-4 lg:p-16 lg:ml-64 mt-[64px] lg:mt-[72px] min-h-screen"
+                >
+                    <div className="">
+                        <Outlet />
+                    </div>
+                </main>
+            </div>
+        </>
+    )
 }
 
-export default AppLayout;
+export default AppLayout
